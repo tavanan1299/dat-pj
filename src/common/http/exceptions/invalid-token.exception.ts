d77dd57bcd6bdb4e4261708f6e14fdc34c1e5a87
nyrmal <taan55374@gmail.com>
2024-05-10 21:55:44 +0700

    Upload images to S3

--- src/common/http/exceptions/invalid-token.exception.ts
+++ src/common/http/exceptions/invalid-token.exception.ts
@@ -3,6 +3,6 @@ import { UnauthorizedException } from '@nestjs/common';
 
 export class InvalidTokenException extends UnauthorizedException {
 	constructor() {
-		super({ errorType: ErrorType.InvalidToken });
+		super({ statusCode: 401, error: 'Unauthorize', message: ErrorType.InvalidToken });
 	}
 }
